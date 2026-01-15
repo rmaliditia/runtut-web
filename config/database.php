00ad88733922b3ccb35d988a2f26a320564d974d
rmaliditia <aliditia123@gmail.com>
2026-01-16 04:26:47 +0700

    TEST ENV

--- config/database.php
+++ config/database.php
@@ -1,5 +1,7 @@
 <?php
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // --- FUNGSI PEMBACA .ENV MANUAL ---
 function loadEnv($path) {
     if (!file_exists($path)) {
